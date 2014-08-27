f0b3baeff7411289dc6e3056130d0797e07510d3
Alexandre Chopin <wikeo65@gmail.com>
2014-08-27 19:12:42 +0200

    .index

--- public/index.php
+++ public/index.php
@@ -11,10 +11,6 @@ require_once(ROOT.DS.'private'.DS.'core'.DS.'includes.php');
         <meta charset="utf-8">
         <title><?= App::$vars['title'];?></title>
         <meta name="description" content="">
-		<link rel="stylesheet" type="text/css" media="screen" href="css/font-awesome.css">
-		<!--[if lt IE 9]>
-			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
     </head>
 	<body>
 		Home page
