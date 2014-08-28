09907f301980cba03714a0b7c142db6828544bdb
Alexandre Chopin <wikeo65@gmail.com>
2014-08-28 14:45:41 +0200

    .debug opt

--- private/controllers/User.php
+++ private/controllers/User.php
@@ -16,7 +16,7 @@ class User {
 	public function update() {
 		$data = array();
 		parse_str(file_get_contents("php://input"), $data);
-		$opt = (isset(App::$vars['params']['id'])) ? '["_id eq '.App::$vars['params']['id'].'"]' : null;
+		$opt = (isset(App::$vars['params']['id'])) ? array('_id eq '.App::$vars['params']['id']) : null;
 		$users = new Database('admin','users');
 		if ($data && !($err = $users->update($opt, $data, Prototype::$user))) {
 			App::send(E200);
@@ -24,7 +24,7 @@ class User {
 		App::send(E400, $err);
 	}
 	public function delete() {
-		$opt = (isset(App::$vars['params']['pseudo'])) ? '["pseudo eq '.App::$vars['params']['pseudo'].'"]' : null;
+		$opt = (isset(App::$vars['params']['pseudo'])) ? array('pseudo eq '.App::$vars['params']['pseudo']) : null;
 		$users = new Database('admin','users');
 		if ($users->delete($opt)) {
 			App::send(E200);
